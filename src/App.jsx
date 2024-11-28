4c5cdfb2049f154ce664f4a06cfb82e4828bbe9c
AmrMahmoud <dramr2852001@gmail.com>
2024-11-28 22:05:28 +0300

    added the login and logout handlers

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,26 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import History from "./pages/History";
-import "./app.css"
+import "./app.css";
 import ProtectedRoute from "./components/ProtectedRoute";
+import { AuthProvider } from "./auth/context/AuthContext";
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/history" element={
-          <ProtectedRoute>
-            <History />
-          </ProtectedRoute>} />
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/history"
+            element={
+              <ProtectedRoute>
+                <History />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   );
 };
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,4 @@
 import { Link } from "react-router-dom";
-import History from "../pages/History";
-import Login from "../pages/Login";
 const Nav = () => {
   return (
     <div>
