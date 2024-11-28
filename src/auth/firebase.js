4c28cfeccbb3dbefbdcd75f849a6c8bed14c51ec
saher marsi <saherkau1@gmail.com>
2024-11-28 20:35:59 +0300

    Added authentication folder which consists of auth context, and firebase js file. Also Protected Route file!!!

---
+++ src/auth/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCWbQRGP-730J6SoJIg8B8DLP2T1K8ToSo",
+  authDomain: "cpit405-groupproject.firebaseapp.com",
+  projectId: "cpit405-groupproject",
+  storageBucket: "cpit405-groupproject.firebasestorage.app",
+  messagingSenderId: "689136199596",
+  appId: "1:689136199596:web:84e620167533690479fc09"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
