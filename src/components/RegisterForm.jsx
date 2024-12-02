60ea1a4827d2ad457d012d2aa0aa2a8f7f5523fc
saher marsi <saherkau1@gmail.com>
2024-12-02 10:54:38 +0300

    RegisterForm updated where you can now create an account successfuly and securely

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,23 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import InputBox from "./InputBox";
 import { FaUser, FaLock, FaEnvelope } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../auth/context/AuthContext";
 
-const RegisterForm = ({handleLinkClick}) => {
+const RegisterForm = ({ handleLinkClick }) => {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState("");
+  const { signup } = useAuth();
+  const navigate = useNavigate();
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try {
+      await signup( email, password);
+      navigate("/");
+    } catch (error) {
+      setError("Failed to signup");
+    }
+  }
+
   return (
     <div className="form-box register">
-      <form action="">
+      {error && <div style={{ color: "red" }}>{error}</div>}
+      <form onSubmit={handleSubmit}>
         <h1>Sign Up</h1>
         <InputBox
           placeholder={"Username"}
           type={"text"}
           isRequired={true}
           Icon={FaUser}
+          value={username}
+          setValue={setUsername}
         />
         <InputBox
           placeholder={"Email"}
           type={"email"}
           isRequired={true}
           Icon={FaEnvelope}
+          value={email}
+          setValue={setEmail}
         />
         <InputBox
           placeholder={"Password"}
@@ -25,12 +49,17 @@ const RegisterForm = ({handleLinkClick}) => {
           isRequired={true}
           Icon={FaLock}
           minLength={8}
+          value={password}
+          setValue={setPassword}
         />
 
         <button type="submit">Sign Up</button>
 
         <div className="register-link">
-          <p>Already have an account? <span onClick={handleLinkClick}>Login</span></p>
+          <p>
+            Already have an account?{" "}
+            <span onClick={handleLinkClick}>Login</span>
+            </p>
         </div>
       </form>
     </div>
