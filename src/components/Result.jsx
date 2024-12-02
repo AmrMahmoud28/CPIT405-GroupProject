2c7e1848e135476a45f8623828037ea2e6b8be78
AmrMahmoud <dramr2852001@gmail.com>
2024-12-02 14:20:19 +0300

    added a not complete style to the result component

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,15 +1,48 @@
-import React from 'react'
+import React from "react";
 
-const Result = ({payload}) => {
-  console.log(JSON.stringify(payload))
+const Result = ({ payload }) => {
+  console.log(JSON.stringify(payload));
+  const productName = payload.product?.product_name || "Unknown Product";
+  const imageUrl =
+    payload.product?.image_url || "https://via.placeholder.com/600";
+  const isHalal = true;
+
+  const allergensList = payload.product?.allergens_hierarchy || [];
+  const formattedAllergens =
+    allergensList.length > 0
+      ? allergensList.map((allergen) =>
+          allergen.replace("en:", "").replace(/_/g, " ").toUpperCase()
+        )
+      : ["No allergens specified"];
+
   return (
-    <div>
-      <p>{
-      JSON.stringify(payload)
-      //uncomment this to check the result or print it in the console 👍
-      }</p>
-    </div>
-  )
-}
+    <div className="cardWrapper">
+      <div className="content">
+        <div className="card">
+          <div className="imageContent">
+            <span className="overlay"></span>
+            <div className="cardImage">
+              <img src={imageUrl} alt={productName} className="cardImg" />
+            </div>
+          </div>
 
-export default Result
+          <div className="cardContent">
+            <h1 className="name">{productName}</h1>
+            <h2 className="halal">{`${isHalal ? "Halal" : "Haram"}`}</h2>
+            <div className="allergens">
+              <h2>Allergens:</h2>
+              <ul>
+                {formattedAllergens.map((allergen, index) => (
+                  <li key={index}>{allergen}</li>
+                ))}
+              </ul>
+            </div>
+            <button className="button">View Nutrition</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Result;
